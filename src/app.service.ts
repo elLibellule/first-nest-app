6da41aa59896b5de13fdaac5ce16007ef10718a2
jean <jean_gabriel@gmx.com>
2024-09-26 05:05:49 +0200

    update app service getHello

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Hello World, It's our First nestjs App !";
+    return "Hello World, this is just awesome !! I'm so glad, ";
   }
 }
